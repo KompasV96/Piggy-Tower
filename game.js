80e54d82788bc618683eedbfb0bc830167da41f7
KompasV96 <krystiankowalczyk1996@gmail.com>
2026-02-24 23:38:14 +0100

    Update game.js

--- game.js
+++ game.js
@@ -344,7 +344,7 @@ if(boostLockTimer > 0){
   boostLockTimer -= dtSec;
 }
 
-player.vx += (targetSpeed - player.vx) * 8 * control * dtSec;
+player.vx += (targetSpeed - player.vx) * 12 * control * dtSec;
 
   player.x += player.vx*dtSec;
   if(player.x<0) player.x=0;
@@ -553,3 +553,4 @@ function loop(now){
 }
 
 requestAnimationFrame(loop);
+
